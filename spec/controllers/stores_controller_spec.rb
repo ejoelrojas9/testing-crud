957792c3401c6966b4452f60d539c0f7c1102149
ejoelrojas9 <96266652+ejoelrojas9@users.noreply.github.com>
2023-05-14 22:55:40 -0500

    Commets added in stores_controller_spec file

--- spec/controllers/stores_controller_spec.rb
+++ spec/controllers/stores_controller_spec.rb
@@ -3,42 +3,42 @@ require 'rails_helper'
 RSpec.describe StoresController, type: :controller do
   let(:store) { create(:store) }
 
-  describe 'GET #index' do  # Modulo y descripción del test en este caso Index
+  describe 'GET #index' do  # Módulo y descripción del test en este caso Index
     it "renders the index template" do  # Inicio individual del test
-      get :index  # Acción o metodo del controlador que se va a testear
+      get :index  # Acción o método del controlador que se va a testear
       # Función que compara el resultado que se espera recibir
       expect(response).to render_template(:index)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET #show' do  # Inicio del modulo de prueba para el metodo Show
-    let(:params) do  # Variable local de los parametros que recibe el test
+  describe 'GET #show' do  # Inicio del módulo de prueba para el método Show
+    let(:params) do  # Variable local de los parámetros que recibe el test
       { 
         id: store.id
       }
     end
-    it "render the show templae" do  # Inicio individual del test
-      get :show, params: params  # Asignación del metodo y parametros
+    it "render the show template" do  # Inicio individual del test
+      get :show, params: params  # Asignación del método y parámetros
       # Función que compara el resultado que se espera recibir
       expect(response).to render_template(:show)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'GET #new' do # Inicio del modulo de prueba para el metodo New
+  describe 'GET #new' do # Inicio del módulo de prueba para el método New
     it "render the new template" do # Inicio individual del test
-      get :new  # Asignación del metodo
+      get :new  # Asignación del método
       # Función que compara el resultado que se espera recibir
       expect(response).to render_template(:new)
     end
   end
 
-  describe 'POST #create' do # Inicio del modulo de prueba para el metodo Create
-    let(:params) do # Variable local de los parametros que recibe el test
+  describe 'POST #create' do # Inicio del módulo de prueba para el método Create
+    let(:params) do # Variable local de los parámetros que recibe el test
       { 
         store: {
-          # Datos aleatorios locales para el modulo
+          # Datos aleatorios locales para el módulo
           article: Faker::Device.model_name,
           state: %w(New Uses Damaged Repaired).sample,
           in_warehouse: Faker::Number.between(from: 1, to: 100),
@@ -50,7 +50,7 @@ RSpec.describe StoresController, type: :controller do
       } 
     end
     it "create the new store and render the show view" do # Inicio individual del test
-      post :create, params: params # Asignación del metodo y parametros
+      post :create, params: params # Asignación del método y parámetros
       store = Store.last  # Asignación del objeto creado en el test
       # Comparación de un dato, el del objeto creado y el que viene en el parametro
       expect(store.article).to eq(params[:store][:article])
@@ -60,26 +60,26 @@ RSpec.describe StoresController, type: :controller do
     end
   end
 
-  describe 'GET #edit' do # Inicio del modulo de prueba para el metodo Edit
-    let(:params) do # Variable local de los parametros que recibe el test
+  describe 'GET #edit' do # Inicio del módulo de prueba para el método Edit
+    let(:params) do # Variable local de los parámetros que recibe el test
       { 
         id: store.id
       }
     end
     it "render the store form" do # Inicio individual del test
-      get :edit, params: params # Asignación del metodo y parametros
+      get :edit, params: params # Asignación del método y parámetros
       # Función que compara el resultado que se espera recibir
       expect(response).to render_template(:edit)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe 'PUT #update' do  # Inicio del modulo de prueba para el metodo Update
-    let(:params) do # Variable local de los parametros que recibe el test
+  describe 'PUT #update' do  # Inicio del módulo de prueba para el método Update
+    let(:params) do # Variable local de los parámetros que recibe el test
     { 
       id: store.id,
       store: {
-        # Datos aleatorios locales para el modulo
+        # Datos aleatorios locales para el módulo
         article: Faker::Device.model_name,
         state: %w(New Uses Damaged Repaired).sample,
         in_warehouse: Faker::Number.between(from: 1, to: 100),
@@ -91,9 +91,9 @@ RSpec.describe StoresController, type: :controller do
     }
     end
     it "update the store and render the template show store" do # Inicio individual del test
-      put :update, params: params # Asignación del metodo y parametros
+      put :update, params: params # Asignación del método y parámetros
       # Comparación de un dato, el del objeto creado y el que viene en el parametro
-      # En este caso store.reload evita recibir datos de otro modulo de prueba
+      # En este caso store.reload evita recibir datos de otro módulo de prueba
       expect(store.reload.article).to eq(params[:store][:article])
       expect(store.reload.in_warehouse).to eq(params[:store][:in_warehouse])
       # Comparación del redireccionamiento una vez actualizado el objeto
@@ -101,14 +101,14 @@ RSpec.describe StoresController, type: :controller do
     end
   end
 
-  describe 'DELETE #destroy' do # Inicio del modulo de prueba para el metodo Destroy
-    let(:params) do # Variable local de los parametros que recibe el test
+  describe 'DELETE #destroy' do # Inicio del módulo de prueba para el método Destroy
+    let(:params) do # Variable local de los parámetros que recibe el test
       {  
         id: store.id
       }
     end
     it "render template index" do # Inicio individual del test
-      delete :destroy, params: params # Asignación del metodo y parametros
+      delete :destroy, params: params # Asignación del método y parámetros
       # Comparación del redireccionamiento una vez eliminado el objeto
       expect(response).to redirect_to(stores_path)
     end
